da7b66db77b05bbc062ee42836737aef08f3f22a
Nika Doghonadze <ndogh13@freeuni.edu.ge>
2016-05-06 01:06:05 +0400

    Add test using mockito framework

--- src/main/java/Doubler.java
+++ src/main/java/Doubler.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Nika Doghonadze
  */
 public class Doubler {
     public int act(int n) {
         return n*2;
+    }
+
+    public int act(List list) {
+        return list.size()*2;
     }
 }
--- src/test/java/DoublerTest.java
+++ src/test/java/DoublerTest.java
@@ -1,18 +1,33 @@
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Nika Doghonadze
  */
 public class DoublerTest {
     @Test
-    public void act() throws Exception {
+    public void testActOnInt() {
         Doubler doubler = new Doubler();
 
         int res = doubler.act(2);
 
         assertEquals(4, res);
+    }
+
+    @Test
+    public void testActOnList() {
+        List list = Mockito.mock(List.class);
+        when(list.size()).thenReturn(12);
+        Doubler doubler = new Doubler();
+
+        int res = doubler.act(list);
+
+        assertEquals(24, res);
     }
 
 }
